e4c1040a1fde042fd219abcb9bb44172c35d0eb0
Jose Ricardo <josericardo29@hotmail.es>
2019-07-14 17:32:53 -0400

    Se ha implementado la calse universidad

--- MasterProject/GradeItWebApp/src/main/java/com/falcontech/gradeit/models/University.java
+++ MasterProject/GradeItWebApp/src/main/java/com/falcontech/gradeit/models/University.java
@@ -1,3 +1,40 @@
 package com.falcontech.gradeit.models;
 
-public class University { }
+import java.util.List;
+
+public class University {
+    private String name;
+    private List<Career> careers;
+    private String indexType;
+    
+    public University() {}
+
+    public University(String name, List<Career> careers, String indexType) {
+        setName(name);
+        setCareers(careers);
+        setIndexType(indexType);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getIndexType() {
+        return indexType;
+    }
+
+    public void setIndexType(String indexType) {
+        this.indexType = indexType;
+    }
+
+    public List<Career> getCareers() {
+        return careers;
+    }
+
+    public void setCareers(List<Career> careers) {
+        this.careers = careers;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
